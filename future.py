80e55a9512d08c06a0ff730e1ef90c5173e599e3
Kartik Chopra <k5chopra@uwaterloo.ca>
2021-05-08 12:02:23 -0400

    Fix bug issues

--- future.py
+++ future.py
@@ -4,20 +4,20 @@ from functools import lru_cache
 import time
 
 def future(func, paramList):
-    return cf.ProcessPoolExecutor(max_workers=len(tags)).map(func, paramList)
+    return cf.ProcessPoolExecutor(max_workers=len(paramList)).map(func, paramList)
 
-@lru_cache
+@lru_cache(maxsize=4)
 def query(url):
     req = requests.get(url)
-    return req.json()
+    return req
 
 def query_non_cahced(url):
     req = requests.get(url)
-    return req.json()
+    return req
 
-urls = ['www.ckartik.com', 'http://ckartik.com/posts/os-concepts-common/', 'http://ckartik.com/about/', 'http://ckartik.com/posts/exception-handling/']
+urls = ['http://www.ckartik.com', 'http://ckartik.com/posts/os-concepts-common/', 'http://ckartik.com/about/', 'http://ckartik.com/posts/exception-handling/']
 start = time.time()
-posts = [post for post in query_future(query, urls)]
+posts = [post for post in future(query, urls)]
 end = time.time()
 
 print("The time to query the URLs in concurrent fashion took: {}".format(end-start))

a930b9ad942aa235f1c4924f8f96d58aadbf9d6d
Kartik Chopra <k5chopra@uwaterloo.ca>
2021-05-08 12:00:35 -0400

    Adds futures lib

---
+++ future.py
@@ -0,0 +1,29 @@
+import requests
+import concurrent.futures as cf
+from functools import lru_cache
+import time
+
+def future(func, paramList):
+    return cf.ProcessPoolExecutor(max_workers=len(tags)).map(func, paramList)
+
+@lru_cache
+def query(url):
+    req = requests.get(url)
+    return req.json()
+
+def query_non_cahced(url):
+    req = requests.get(url)
+    return req.json()
+
+urls = ['www.ckartik.com', 'http://ckartik.com/posts/os-concepts-common/', 'http://ckartik.com/about/', 'http://ckartik.com/posts/exception-handling/']
+start = time.time()
+posts = [post for post in query_future(query, urls)]
+end = time.time()
+
+print("The time to query the URLs in concurrent fashion took: {}".format(end-start))
+
+start = time.time()
+posts = [query_non_cahced(post) for post in urls]
+end = time.time()
+
+print("The time to query the URLs in regular fashion took: {}".format(end-start))
